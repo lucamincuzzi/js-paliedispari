acf1571fc95269fe721f12d1f9736280691cc019
lucamincuzzi <luca.mincuzzi@outlook.it>
2023-10-26 18:45:29 +0200

    update: Palindromi - reverseStr function in js

--- Palindromi/js/script.js
+++ Palindromi/js/script.js
@@ -1,21 +1,39 @@
 // Codice da eseguire
 const userStr = prompt("Dimmi una parola");
-const userRevStr = reverseStr(userStr);
-console.log(userRevStr);
+let userRevStr = "";
+const isItPalindrome = reverseStr(userStr)
+console.log("Stringa utente:", userStr, "Stringa utente al contrario:", userRevStr, "Esito:", isItPalindrome);
 
 /**********/
 /* Descrizione: data una stringa, la riscrive al contrario in un'altra variabile string
 /* @param {userStr}
-/* @returns {string}
+/* @returns {string, string}
 */
 
 function reverseStr(userStr) {
-  // creo una variabile string vuota
-  let reversedStr = "";
-  // ciclo for che scorre la stringa parametro al contrario partendo dall'ultimo carattere come fosse un array.
+  // ciclo for che scorre la stringa parametro al contrario, partendo dall'ultimo carattere come fosse un array
   for (let i = userStr.length - 1; i >= 0; i--) {
     // scrivo il carattere all'indice dell'iterazione nella variabile dichiarata nella funzione
-    reversedStr += userStr[i];
+    userRevStr += userStr[i];
   }
-  return reversedStr; // restituisco il valore della string a fine ciclo
+  // creo un flag booleano per la verifica della parola palindroma
+  let palindromo = false;
+  // creo il messaggio che comunica l'esito del controllo
+  result = "";
+  // istruzione condizionale per la verifica della parola palindroma
+  // se la parola dell'utente è uguale a quella riscritta al contrario
+  if (userStr === userRevStr) {
+    // il flag diventa true
+    palindromo = true;
+  }
+  // se il flag è true
+  if (palindromo === true) {
+    // scrivo nel messaggio
+    result += "È palindroma!";
+  } else {
+    //altrimenti scrivo
+    result += "Non è palindroma...";
+  }
+  // restituisco il valore della stringa creata a fine ciclo e l'esito della verifica
+  return userRevStr, result;
 }
